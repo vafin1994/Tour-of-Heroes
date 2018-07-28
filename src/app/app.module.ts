f7e40476de0841dfb9c8b8196f15aaef597ee80f
vafin1994 <vafinalex@gmail.com>
2018-07-28 18:16:39 +0300

    Add search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {HeroesComponent} from './heroes/heroes.component';
 import {MessagesComponent} from './messages/messages.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   imports: [
@@ -31,6 +32,7 @@ import {MessagesComponent} from './messages/messages.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    HeroSearchComponent,
   ],
   bootstrap: [AppComponent]
 })
